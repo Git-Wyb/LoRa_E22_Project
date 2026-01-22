06fa1f837029f2c094568bc6d574516915a2e3ec
myvsgit <qlwyb@aoyagi-cq.com>
2026-01-22 14:16:20 +0800

    2026.01.22   当装上天线距离很近信号很强，LCD显示-256，显示BUG，修正为显示0dBm。

--- user_src/display.c
+++ user_src/display.c
@@ -70,7 +70,8 @@ void Display_refresh(u8 x,u8 y,u8 snum)
     Display_String(x+11*7,snum*ycoe+y,charbuf,strlen(charbuf));
 
     memset(charbuf,0,sizeof(charbuf));
-    snprintf(charbuf,20,"-%ddBm",(256-SlaNumStu[snum].rssi));
+    if(SlaNumStu[snum].rssi == 0) snprintf(charbuf,20,"%ddBm",(0));
+    else snprintf(charbuf,20,"-%ddBm",(256-SlaNumStu[snum].rssi));
     Display_String(start_x,7*ycoe+y,"        ",8);
     Display_String(start_x,7*ycoe+y,charbuf,strlen(charbuf));
 
